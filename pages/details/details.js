28468ae66d9cbd50f5c647ddb51cc749f9bc4483
weifo <1641929622@qq.com>
2020-03-03 00:05:03 +0800

    增加评论模块

--- pages/details/details.js
+++ pages/details/details.js
@@ -5,16 +5,52 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    recommandedList:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // options 可以拿到url中传递的参数
+    let videoId=options.id;
+    this.getCurrentVideo(videoId);
+    this.getOtherList(videoId);
+    this.getCommentList(videoId);
   },
+  getCurrentVideo(videoId){
+    wx.request({
+      url:'xxx/bili?id='+videoId,
+      success(res){
+        if(res.statusCOde===200){
 
+        }
+      }
+    })
+  },
+  getOtherList(videoId){
+    let _this=this;
+    ex.request({
+      url:'XXX.com'+videoId,
+      success(res){
+        _this.setData({
+          recommandedList:res.data.data.otherList
+        })
+      }
+    })
+  },
+  getCommentList(videoId){
+    wx.request({
+      url:'xxx',
+      success(res){
+        if(res.statusCode===200){
+          this.setData({
+            comments:res.data.commentList
+          })
+        }
+      }
+    })
+  }
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
